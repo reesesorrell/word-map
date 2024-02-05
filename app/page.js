dc67fb0d3186aef3041f931cc80d8917b09ed8dc
Reese Sorrell <joreesesorrell@gmail.com>
2024-02-05 12:52:58 -0600

    Update trie json with a better list

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import next from 'next';
 import HomePage from './client-home-page';
 
 export default async function Page() {
-  const file = await fs.readFile(process.cwd() + '/app/words_alpha_trie.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/app/generated_words_trie.json', 'utf8');
   const data = JSON.parse(file);
-  const dict = data['x']['s']
+  const dict = data['S']['T']['A']['R']
 
   return (
     <div>
       <HomePage dictData={data} />
-      {/*<div>{Object.keys(dict)}</div>*/}
+      {<div>{Object.keys(dict)}</div>}
     </div>
   );
 }
